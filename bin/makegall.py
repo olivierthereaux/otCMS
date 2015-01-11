459561e4f34329c8d5d9e077d6a477abc8dcbcac
Olivier Thereaux <ot@zoy.org>
2015-01-11 18:09:10 +0000

    adding inline photo markup option

--- bin/makegall.py
+++ bin/makegall.py
@@ -11,6 +11,17 @@ Created by Olivier Thereaux on 2012-07-11.
 import sys
 import os
 import re
+import getopt
+
+help_message = '''
+makegall.py - Find jpg fnames in current dir and output basic gallery markup
+
+Options:
+    --inline    output markup to display images in the post 
+                (default is to have thumbnails)
+    -h          this help message
+'''
+
 
 def readRDF(fname):
     """Yes, this is how I parse my PhotoRDF. Sue me."""
@@ -32,23 +43,63 @@ def readRDF(fname):
     return title, description
     
 
-def main():
-    print '<div class="gall">'
-    dirList=os.listdir(".")
-    for fname in dirList:
-        if re.search(r".jpg$", fname):
-            file_base = re.sub(r"\..*$", "", fname)
-            alt_text = ''
-            link_text = ''
-            title_text = ''
-            rdf_fname = file_base+".rdf"
-            if rdf_fname in dirList:
-                title, description = readRDF(rdf_fname)
-                alt_text = "Photo: "+title
-                title_text = description
-                link_text = title
-            print '''<div class="gallone"><a href="%(fname)s.html" title=""><img src="tn/tn_%(fname)s.jpg" alt="%(alt_text)s" title="%(title_text)s" /></a><p><a href="%(fname)s.html">%(link_text)s</a></p></div>''' % {"fname": fname, 'alt_text': alt_text, "link_text": link_text, "title_text": title_text}
-    print '</div>'
+def main(argv=None):
+    inline = False
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "h", ["help", "inline"])
+        except getopt.error, msg:
+            raise Usage(msg)
+    
+        # option processing
+        for option, value in opts:
+            if option == "--inline":
+                inline = True
+            if option in ("-h", "--help"):
+                raise Usage(help_message)
+    
+    except Usage, err:
+        print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+        print >> sys.stderr, "\t for help use --help"
+        return 2
+    
+    if inline:
+        dirList=os.listdir(".")
+        for fname in dirList:
+            if re.search(r".jpg$", fname):
+                file_base = re.sub(r"\..*$", "", fname)
+                alt_text = ''
+                link_text = ''
+                title_text = ''
+                rdf_fname = file_base+".rdf"
+                if rdf_fname in dirList:
+                    title, description = readRDF(rdf_fname)
+                    alt_text = "Photo: "+title
+                    title_text = description
+                    link_text = title
+                print '''<div class="picCenter picCaption">
+    <img src="%(fname)s" alt="%(alt_text)s" />
+    <p>%(title_text)s</p>
+</div>''' % {"fname": fname, 'alt_text': alt_text, "link_text": link_text, "title_text": title_text}
+    else:
+        print '<div class="gall">'
+        dirList=os.listdir(".")
+        for fname in dirList:
+            if re.search(r".jpg$", fname):
+                file_base = re.sub(r"\..*$", "", fname)
+                alt_text = ''
+                link_text = ''
+                title_text = ''
+                rdf_fname = file_base+".rdf"
+                if rdf_fname in dirList:
+                    title, description = readRDF(rdf_fname)
+                    alt_text = "Photo: "+title
+                    title_text = description
+                    link_text = title
+                print '''<div class="gallone"><a href="%(fname)s.html" title=""><img src="tn/tn_%(fname)s.jpg" alt="%(alt_text)s" title="%(title_text)s" /></a><p><a href="%(fname)s.html">%(link_text)s</a></p></div>''' % {"fname": fname, 'alt_text': alt_text, "link_text": link_text, "title_text": title_text}
+        print '</div>'
 
 if __name__ == '__main__':
     main()
